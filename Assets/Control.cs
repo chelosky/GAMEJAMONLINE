471ac11102ffde85dc344d171726d370931b9344
Ignacio Munoz <ignacio.alejandro.797@gmail.com>
2020-09-13 16:22:05 -0300

    Menu

---
+++ Assets/Control.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public AudioSource efecto;
+
+    public void CambiarEscena(string nombre)
+    {
+        efecto.Play();
+        SceneManager.LoadScene(nombre);
+    }
+    public void Salir()
+    {
+        efecto.Play();
+        Application.Quit();
+    }
+}
